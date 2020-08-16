a2cdd0d5f0aca42bb5c60b95bdf7946cdc4bdebe
bake0937 <sadayasu.info@gmail.com>
2020-08-16 12:40:50 +0900

    タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Apps
+module Baukis2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.time_zone = "Tokyo"
+      config.i18n.load_path +=
+        Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.default_locale = :ja
   end
 end
