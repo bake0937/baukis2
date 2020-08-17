e19d0694764bfcf44f50af404f33173a6258c128
okabeeeat <sadayasu.info@gmail.com>
2020-08-17 22:35:09 +0900

    fix coding standard

--- spec/experiments/string_spec.rb
+++ spec/experiments/string_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe String do
   describe '#<<' do
-    example "文字の追加" do
-      s = "ABC"
-      s << "D"
+    example '文字の追加' do
+      s = 'ABC'
+      s << 'D'
       expect(s.size).to eq(4)
     end
 
